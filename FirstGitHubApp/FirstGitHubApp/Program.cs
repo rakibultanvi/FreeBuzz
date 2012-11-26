498a08b90d747dd972071d89a5b6461053b302dd
Rakibul Alam <mailfortanvi@gmail.com>
2012-11-26 19:02:40 +0600

    write some sample code

--- FirstGitHubApp/FirstGitHubApp/Program.cs
+++ FirstGitHubApp/FirstGitHubApp/Program.cs
@@ -9,6 +9,24 @@ namespace FirstGitHubApp
     {
         static void Main(string[] args)
         {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                if (i % 3 == 0 && i%5==0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+ 
+            }
+            Console.ReadKey();
         }
     }
 }
